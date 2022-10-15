2bded15ef77361246fa19f4d40488e306839e2a4
Ignacio Granados <ignaciogramar@gmail.com>
2022-10-14 20:32:33 -0600

    Adding exercises

---
+++ saxpy.cu
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+__global__
+void saxpy(int n, float a, float *x, float *y)
+{
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  if (i < n) y[i] = a*x[i] + y[i];
+}
+
+//CPU function
+void saxpy_h(int n, float a, float *x, float *y){
+    for(int i = 0; i < n; i++)
+        y[i] = a*x[i] + y[i];
+}
+
+int main(void)
+{
+  int N = 1<<20;
+  float *x, *y, *d_x, *d_y;
+  x = (float*)malloc(N*sizeof(float));
+  y = (float*)malloc(N*sizeof(float));
+
+  cudaMalloc(&d_x, N*sizeof(float)); 
+  cudaMalloc(&d_y, N*sizeof(float));
+
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
+
+  // Perform SAXPY on 1M elements 
+  clock_t start_d=clock();
+  printf("Doing GPU Vector SAXPY\n");
+  saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
+
+  cudaMemcpy(y, d_y, N*sizeof(float), cudaMemcpyDeviceToHost);
+  clock_t end_d = clock();
+
+    printf("Doing CPU Vector add\n");
+    clock_t start_h = clock();
+    saxpy_h(N, 2.0f, x, y);
+    clock_t end_h = clock();
+
+    //Time computing
+    double time_d = (double)(end_d-start_d)/CLOCKS_PER_SEC;
+    double time_h = (double)(end_h-start_h)/CLOCKS_PER_SEC;
+
+    //Copying data back to host, this is a blocking call and will not start until all kernels are finished
+    printf("\t GPU time = %f \t CPU time = %f\n", time_d, time_h);
+
+
+
+  cudaFree(d_x);
+  cudaFree(d_y);
+  free(x);
+  free(y);
+}
